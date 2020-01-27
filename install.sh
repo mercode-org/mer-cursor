8d45ae6ff702608c82ed18ad7906e40c83fb576e
Yutyo <tanakinci2002@gmail.com>
2020-01-27 14:41:57 +0300

    Added installer and changed readme.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+gh_repo="mercode-jbrawn"
+gh_desc="Mercode-Jbrawn"
+
+echo "Copyleft 2019-2020 Mercode"
+git pull origin master
+sudo mkdir -p /usr/share/icons/mer-cursor
+sudo cp -r -f ./ /usr/share/icons/mer-cursor/
+echo Done!
